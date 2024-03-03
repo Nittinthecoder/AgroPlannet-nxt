47d65e374f60f80d9a7bddfa9d27916e09ba1099
Nittinthecoder <93333941+Nittinthecoder@users.noreply.github.com>
2024-03-03 12:47:14 +0530

    normal

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 // import Image from 'next/image'
 // import {MenuIcon, SearchIcon, ShoppingCartIcon} from "heroicons/react/outline"
 import { IoSearch } from "react-icons/io5";
-import { CiShoppingCart } from "react-icons/ci";
+import { HiOutlineShoppingCart } from "react-icons/hi";
 
 
 const Header = () => {
@@ -24,31 +24,32 @@ const Header = () => {
                     {/* if no logo use this */}
 
                     <div className='mt-2  flex items-center flex-grow sm:flex-grow-0 '>
-                        <h2 className='font-medium text-1xl'> ArgoXplanet </h2>
+                        <h2 className='font-medium text-1xl mb-[7px]'> ArgoXplanet </h2>
                     </div>
 
                     {/* Search bar */}
 
                     <div className= 'hidden sm:flex items-center h-10 ml-2 rounded-md flex-grow cursor-pointer bg-lime-400 hover:bg-lime-600'>
-                        <input className='p-2 h-full  flex-grow flex-shrink rounded-md focus:outline-none' type="text" />
-                        <IoSearch  className='h-8 '/>
+                        <input className='p-2 h-full  flex-grow flex-shrink rounded-l-md focus:outline-none' type="text" />
+                        <IoSearch  className='text-2xl ml-[10px] '/>
                     </div>
 
                     {/* right side  */}
 
-                    <div className='text-white'>
-                        <div>
+                    <div className='text-black flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+                        <div className='link'>
                             <p>Hello Nittin Nobby Mathew</p>
                             <p>Accounts & List</p>
                         </div>
 
-                        <div>
+                        <div className='link'>
                             <p>Returns</p>
                             <p>& Orders</p>
                         </div>
 
-                        <div>
-                            <CiShoppingCart />
+                        <div className='link'>
+                            <HiOutlineShoppingCart className='text-2xl'/>
+                            <p>Basket</p>
                         </div>
                     </div>
                 </div>
